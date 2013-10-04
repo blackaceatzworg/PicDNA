3042025f1618ef860a38abad57d02184a17c63f7
Gael Abadin <gael.abadin@gmail.com>
2013-10-04 03:20:58 +0200

    First commit.

---
+++ src/ds_memes_br/factories/ReLogoPanelCreator.java
@@ -0,0 +1,15 @@
+package ds_memes_br.factories;
+
+import javax.swing.JPanel;
+import repast.simphony.relogo.factories.AbstractReLogoPanelCreator;
+import ds_memes_br.relogo.UserGlobalsAndPanelFactory;
+
+public class ReLogoPanelCreator extends AbstractReLogoPanelCreator {
+
+	public void addComponents(JPanel parent) {
+		UserGlobalsAndPanelFactory ugpf = new UserGlobalsAndPanelFactory();
+		ugpf.initialize(parent);
+		ugpf.addGlobalsAndPanelComponents();
+	}
+
+}
